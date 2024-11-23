d88e0f2b0376ad64d8c5c5744ff5bdb044a46fa9
moe-ky <moyinkoiki@gmail.com>
2024-11-23 20:57:27 +0000

    update contact funtion

--- js/lib.js
+++ js/lib.js
@@ -47,7 +47,6 @@ window.onload = function() {
     populateMenuItem("Drinks", "Others")
     
   }
-  
 };
 
 const populateMenuItem = (category, subCategory) =>{
@@ -74,26 +73,4 @@ const populateMenuItem = (category, subCategory) =>{
       categoryItemDiv.appendChild(elementDiv)
     })
     document.getElementById('food-menu-container').appendChild(categoryItemDiv);
-} 
-
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-  }
-
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+}
