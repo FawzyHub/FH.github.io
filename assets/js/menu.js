c2bd7592d614f3f4867f685adb27db5456396770
moe-ky <moyinkoiki@gmail.com>
2025-08-08 00:08:58 +0100

    updates items

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,13 +1,18 @@
-let menuItems = [];
+// Initialize menuItems as an empty array
+const menuItems = [];
 
 // Fetch menu items from API
 fetch('https://xwer1yi8u0.execute-api.eu-west-1.amazonaws.com/v1/list/items')
   .then(response => response.json())
   .then(data => {
-    menuItems = data;
+    // Update menuItems by pushing data into it
+    items = data.response
+    menuItems.push(...items);
+    console.log('Menu items fetched successfully:', menuItems);
   })
   .catch(error => {
     console.error('Error fetching menu items:', error);
   });
 
+// Export menuItems
 export { menuItems };
