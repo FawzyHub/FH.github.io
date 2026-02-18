b68e3fc7ddac93f097910ba77c85bd356524167c
moe-ky <moyinkoiki@gmail.com>
2026-02-18 20:39:01 +0000

    update prices

---
+++ assets/js/roomDetails.js
@@ -0,0 +1,26 @@
+export const roomDetails = {
+    Classic: {
+        price: 56750,
+        name: "Classic"
+    },
+    DeluxeClassic: {
+        price: 61750,
+        name: "Deluxe Classic"
+    },
+    RoyalDeluxe: {
+        price: 66875,
+        name: "Royal Deluxe"
+    },
+    MarioDoubleLuxury: {
+        price: 71750,
+        name: "Mario Double Luxury"
+    },
+    MakExclusive: {
+        price: 80950,
+        name: "Mak Exclusive"
+    },
+    CindySuite: {
+        price: 130000,
+        name: "Cindy Suite"
+    }
+}
